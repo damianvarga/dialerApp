0867b4cbdec6b3de1a6be5592a43aa50bde25061
Damián Varga <damian.varga@student.tuke.sk>
2021-10-25 18:27:39 +0200

    zaloha

--- dialer/App.js
+++ dialer/App.js
@@ -5,9 +5,10 @@
  * @format
  * @flow strict-local
  */
- 
+
 import React from 'react';
 import type {Node} from 'react';
+import {useState} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -22,6 +23,8 @@ import {
   Pressable,
   Dimensions,
   Image,
+  KeyboardAvoidingViewComponent,
+  
 } from 'react-native';
 import {
   Colors,
@@ -63,17 +66,71 @@ const App: () => Node = () => {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+  /*const [isPressedOne, setIsPressedOne] = useState(false);
+  const [isPressedTwo, setIsPressedTwo] = useState(false);
+  const [isPressedThree, setIsPressedThree] = useState(false);
+  const [isPressedFour, setIsPressedFour] = useState(false);
+  const [isPressedFive, setIsPressedFive] = useState(false);
+  const [isPressedSix, setIsPressedSix] = useState(false);
+  const [isPressedSeven, setIsPressedSeven] = useState(false);
+  const [isPressedEight, setIsPressedEight] = useState(false);
+  const [isPressedNine, setIsPressedNine] = useState(false);
+  const [isPressedHashtag, setIsPressedHashtag] = useState(false);
+  const [isPressedZero, setIsPressedZero] = useState(false);
+  const [isPressedStar, setIsPressedStar] = useState(false);*/
+  //var number = '';
+  // this.state() {
+  //   text: "Click here"
+  // };
+  
+  var addNumber = function(originalText, addedNumber) {
+    // number += addedNumber;
+    // console.log(number);
+    originalText = `${originalText}` + `${addedNumber}`;
+    //originalText = result;
+    return originalText;
+  };
 
+  
+  const [result, setResult] = useState("");
+  const [dial, setDial] = useState(true);
   return (
     <View >
-      <View style={styles.greenArea}></View>
-      <View style={styles.whiteArea}></View>
-
+      <View style={styles.greenArea}>
+        <Text style={styles.whiteText}>Phone</Text>
+      </View>
+      <View style={styles.whiteArea}>
+        <Text style={styles.bigBold}>{result}</Text>
+        
+        
+      
+      </View>
+      <View style={styles.delete}>
+      <Pressable onPress={function() {
+          if (result != "") {
+            var result2 = result.substring(0, result.length-1);
+            console.log(result2);
+            setResult(result2);
+          }
+        }}
+        onLongPress={function(){
+          setResult("");
+        }}>
+          {result != "" ?
+            <Image source={require('./pictures/delete.png')} style={styles.delete}></Image>
+            : null
+          }
+        </Pressable>
+      </View>
       <View style={styles.rowContainer}>
         <Pressable style={styles.button}
-        onPress={function () {
-          console.log("1");
+        onPress={function () {          
+          var result2 = addNumber(result, "1");
+          console.log(result2);
+          setResult(result2);
+          //<Text>1</Text>
         }}>
+        
         <Text style={styles.textUp}>1</Text>
         
         </Pressable>
@@ -81,6 +138,9 @@ const App: () => Node = () => {
         <Pressable style={styles.button}
         onPress={function () {
           console.log("2");
+          var result2 = addNumber(result, "2");
+          console.log(result2);
+          setResult(result2);
         }}>
         <Text style={styles.textUp}>2</Text>
         <Text style={styles.textDown}>ABC</Text>
@@ -89,6 +149,9 @@ const App: () => Node = () => {
         <Pressable style={styles.button}
         onPress={function () {
           console.log("3");
+          var result2 = addNumber(result, "3");
+          console.log(result2);
+          setResult(result2);
         }}>
         <Text style={styles.textUp}>3</Text>
         <Text style={styles.textDown}>DEF</Text>
@@ -98,6 +161,9 @@ const App: () => Node = () => {
         <Pressable style={styles.button}
         onPress={function () {
           console.log("4");
+          var result2 = addNumber(result, "4");
+          console.log(result2);
+          setResult(result2);
         }}>
         <Text style={styles.textUp}>4</Text>
         <Text style={styles.textDown}>GHI</Text>
@@ -106,6 +172,9 @@ const App: () => Node = () => {
         <Pressable style={styles.button}
         onPress={function () {
           console.log("5");
+          var result2 = addNumber(result, "5");
+          console.log(result2);
+          setResult(result2);
         }}>
         <Text style={styles.textUp}>5</Text>
         <Text style={styles.textDown}>JKL</Text>
@@ -114,6 +183,9 @@ const App: () => Node = () => {
         <Pressable style={styles.button}
         onPress={function () {
           console.log("6");
+          var result2 = addNumber(result, "6");
+          console.log(result2);
+          setResult(result2);
         }}>
         <Text style={styles.textUp}>6</Text>
         <Text style={styles.textDown}>MNO</Text>
@@ -123,6 +195,9 @@ const App: () => Node = () => {
         <Pressable style={styles.button}
         onPress={function () {
           console.log("7");
+          var result2 = addNumber(result, "7");
+          console.log(result2);
+          setResult(result2);
         }}>
         <Text style={styles.textUp}>7</Text>
         <Text style={styles.textDown}>PQRS</Text>
@@ -131,6 +206,9 @@ const App: () => Node = () => {
         <Pressable style={styles.button}
         onPress={function () {
           console.log("8");
+          var result2 = addNumber(result, "8");
+          console.log(result2);
+          setResult(result2);
         }}>
         <Text style={styles.textUp}>8</Text>
         <Text style={styles.textDown}>TUV</Text>
@@ -139,6 +217,9 @@ const App: () => Node = () => {
         <Pressable style={styles.button}
         onPress={function () {
           console.log("9");
+          var result2 = addNumber(result, "9");
+          console.log(result2);
+          setResult(result2);
         }}>
         <Text style={styles.textUp}>9</Text>
         <Text style={styles.textDown}>WXYZ</Text>
@@ -148,6 +229,9 @@ const App: () => Node = () => {
         <Pressable style={styles.button}
         onPress={function () {
           console.log("*");
+          var result2 = addNumber(result, "*");
+          console.log(result2);
+          setResult(result2);
         }}>
         <Text style={styles.textUp}>*</Text>
         </Pressable>
@@ -155,19 +239,44 @@ const App: () => Node = () => {
         <Pressable style={styles.button}
         onPress={function () {
           console.log("0");
+          var result2 = addNumber(result, "0");
+          console.log(result2);
+          setResult(result2);
+        }}
+        onLongPress = {function () {
+          var result2;
+          {result === "" ? result2 = addNumber(result, "+") : result2 = addNumber(result, "0") }
+          console.log(result2);
+          setResult(result2);
         }}>
         <Text style={styles.textUp}>0</Text>
+        <Text style={styles.textDown}>+</Text>
         </Pressable>
 
         <Pressable style={styles.button}
         onPress={function () {
           console.log("#");
+          var result2 = addNumber(result, "#");
+          console.log(result2);
+          setResult(result2);
         }}>
         <Text style={styles.textUp}>#</Text>
         </Pressable>
       </View>
       <View style={styles.receiver}>
-        <Image source={require('./pictures/receiver.png')} style={styles.receiver}></Image>
+        <Pressable onPress={function() {
+          result == "" || result == "*" || result == "#" ?
+          setDial(true) :
+          setDial(!dial);
+        }}>
+          
+          <Image source={
+            (dial === true || result == "" || result == "*" || result == "#") ? 
+            require('./pictures/receiver.png'):
+            require('./pictures/redReceiver.png')} style={styles.receiver}></Image>
+          
+        
+        </Pressable>
       </View>
     </View>   
     
@@ -226,10 +335,31 @@ const styles = StyleSheet.create({
     height: Dimensions.get('window').height/6,
     width: Dimensions.get('window').width,
   },
+  whiteText: {
+    color: '#fff',
+    fontSize: 20,
+    paddingLeft: 15,
+    paddingTop: 15,
+  },
   whiteArea: {
     backgroundColor: "white",
-    height: Dimensions.get('window').height/4,
+    height: Dimensions.get('window').height/5,
     width: Dimensions.get('window').width,
+  },
+  bigBold: {
+    color: '#000',
+    fontSize: 28,
+    textAlign: "center",
+    paddingTop: Dimensions.get('window').height/12,
+
+  },
+  delete: {
+    
+    paddingBottom: Dimensions.get('window').height/55,
+    // height: Dimensions.get('window').height/24,
+    // width: Dimensions.get('window').width/8,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   button: {
     height: Dimensions.get('window').height/12,
